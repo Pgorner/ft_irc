537bf119f94bf82fad2b902e7774a16a9918defb
pgorner <pgorner@student.42heilbronn.de>
2023-07-22 16:27:26 +0200

    added small things

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -6,36 +6,11 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 19:07:10 by pgorner           #+#    #+#             */
-/*   Updated: 2023/07/22 16:12:58 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/07/22 16:27:11 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/irc.hpp"
-
-void SetTerminalEcho(bool enable) {
-    struct termios tty;
-    tcgetattr(STDIN_FILENO, &tty);
-    if (enable)
-        tty.c_lflag |= ECHO;
-	else
-        tty.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-}
-
-void SetTerminalNonBlocking(bool nonBlocking) {
-    struct termios ttystate;
-    tcgetattr(STDIN_FILENO, &ttystate);
-
-    if (nonBlocking) {
-        ttystate.c_lflag &= ~(ICANON | ECHO);
-        ttystate.c_cc[VMIN] = 0;
-        ttystate.c_cc[VTIME] = 0;
-    } else {
-        ttystate.c_lflag |= ICANON | ECHO;
-    }
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
-}
 
 void    clear(int i)
 {
@@ -68,10 +43,16 @@ void    write_irc(void){
     }
 }
 
+void proper_exit(void)
+{
+//add proper exit handling
+}
+
 void change_running(int signal)
 {
     if (signal == SIGINT || signal == SIGQUIT){
         running = false;
+        proper_exit();
         goodbye();
         exit(1);
     }

117e87003d5e2994582a9040313015436feee6d3
pgorner <pgorner@student.42heilbronn.de>
2023-08-03 17:26:45 +0200

    USER/NICK/PASS/MODE/OPER working, rmletter(), addmode() added, automatic server registration working, privilege structure corrected, authorisation now required

--- includes/server.hpp
+++ includes/server.hpp
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 16:59:30 by pgorner           #+#    #+#             */
-/*   Updated: 2023/08/02 20:35:45 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/08/03 17:14:04 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,13 @@
     struct ClientData {
         int fd;               // File descriptor for the client
         int passwordAccepted; // Flag indicating if the password is accepted for this client
-        bool cap; // Flag indicating if the password is accepted for this client
+        bool cap; // Flag indicating if CAP has been handled
+        bool auth; // Flag indicating successfull authenticated (NICK & USER)
         std::string mode;       
         std::string nick;
         std::string user;       
         std::string realname;       
-        ClientData(int client_socket, int pwdAccepted, bool cap, std::string mode) : fd(client_socket), passwordAccepted(pwdAccepted), cap(cap), mode(mode) {}
+        ClientData(int client_socket, int pwdAccepted, bool cap, bool auth, std::string mode) : fd(client_socket), passwordAccepted(pwdAccepted), cap(cap), auth(auth), mode(mode) {}
     };
 
 
@@ -50,14 +51,20 @@ class Server {
 	int err(std::string msg);
     void run(); 
     static void change_running(int signal);
-    void checkPwd(const std::vector<std::string>& tokens, int i);
+    void checkPwd(const std::vector<std::string>& tokens, int i, int cc);
     void logsend(int fd, const char* msg, bool servname);
     bool contains(const std::vector<std::string>& tokens, std::string search);
     void cap(int fd, const std::vector<std::string>& tokens, bool cap);
     void commands(int i, int cc, std::vector<std::string> tokens);
-    const char* mode(int cc, std::vector<std::string> tokens);
+    std::string rmletter(char letter, std::string mode);
+    std::string addmode(char letter, std::string mode);
+    
     //cmd functions
+    void nick(std::vector<std::string> tokens, int cc, int i);
+    void user(std::vector<std::string> tokens, int cc, int i);
     int oper(std::vector<std::string> tokens);
+    const char* mode(int cc, std::vector<std::string> tokens);
+    void quit(std::vector<std::string> tokens, int i);
 
     static Server* server_ptr;
 

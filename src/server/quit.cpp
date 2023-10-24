081f267d98b067d275478375a1474e3dde92cee3
pgorner <pgorner@student.42heilbronn.de>
2023-10-24 18:48:34 +0200

    QUIT fixed

--- src/server/quit.cpp
+++ src/server/quit.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgorner <pgorner@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 21:00:50 by pgorner           #+#    #+#             */
-/*   Updated: 2023/10/24 14:09:02 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/10/24 18:48:00 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 void Server::quit(std::vector<std::string> tokens, size_t i, int cc)
 {
-	(void)tokens;
-	std::string resp = ":"SERVERNAME" 302 " + _clients[cc].nick + " :You have quit the server\r\n";
-	logsend(_clients[cc].fd, resp.c_str(), cc);
-	for (size_t i = 0; i < _clients[cc]._channels.size(); i++)
-		removefromchannel(_clients[cc]._channels[i], cc, "");
+    (void)tokens;
+
+    for (size_t j = 0; j < _clients[cc]._channels.size(); j++) {
+        removefromchannel(_clients[cc]._channels[j], cc, "Quitting");
+    }
+    std::string quitMsg = ":" SERVERNAME " 302 " + _clients[cc].nick + " :You have quit the server\r\n";
+    logsend(_clients[cc].fd, quitMsg.c_str(), cc);
+
+    close(_clients[cc].fd);
+    _clients.erase(_clients.begin() + cc);
     _poll_fds.erase(_poll_fds.begin() + i);
-	write_nice(RED, "Client: ", false);
-	write_nice(RED, _clients[cc].nick, false);
-	write_nice(RED, " disconnected", true);
-	_clients.erase(_clients.begin() + cc);
-	close(_poll_fds[i].fd);
+    write_nice(RED, "Client: ", false);
+    write_nice(RED, _clients[cc].nick, false);
+    write_nice(RED, " disconnected", true);
 }

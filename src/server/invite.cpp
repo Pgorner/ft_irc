f6dec0421ca48be852944e085788520fe535a2f9
pgorner <pgorner@student.42heilbronn.de>
2023-10-30 16:09:12 +0100

    stuff fixed

--- src/server/invite.cpp
+++ src/server/invite.cpp
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 20:58:34 by pgorner           #+#    #+#             */
-/*   Updated: 2023/10/30 16:06:13 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/10/30 16:08:39 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,14 @@ void Server::invite(std::vector<std::string> tokens, int cc)
 {
 	if(!check_params(tokens, 3)){_clients[cc].send_to_user += irc::cEM(irc::ERR_NEEDMOREPARAMS("INVITE")); return;};
 	if (!channelexists(tokens[1])){_clients[cc].send_to_user += SERVERNAME" Channel not found\r\n"; return;};
-	if (client_oper(cc, find_chan(tokens[1])))
+	if (_clients[cc].admin == false && !client_oper(cc, find_chan(tokens[1]))){_clients[cc].send_to_user += irc::cEM(irc::ERR_NOPRIVILEGES()); return 0;}
+	int invnick = find_nick(tokens[2]);
+	if (invnick != -1)
 	{
-		int invnick = find_nick(tokens[2]);
-		if (invnick != -1)
-		{
-			_clients[invnick].send_to_user += irc::cEM(irc::RPL_INVITING(_clients[cc].nick, _clients[invnick].nick, tokens[1]));
-			_clients[cc].send_to_user += irc::cEM(irc::RPL_INVITING(_clients[cc].nick, _clients[invnick].nick, tokens[1]));
-			_channels[find_chan(tokens[1])].invited.push_back(invnick);
-		}
-		else
-			_clients[cc].send_to_user += SERVERNAME" USER NOT FOUND\r\n";
-		return;
+		_clients[invnick].send_to_user += irc::cEM(irc::RPL_INVITING(_clients[cc].nick, _clients[invnick].nick, tokens[1]));
+		_clients[cc].send_to_user += irc::cEM(irc::RPL_INVITING(_clients[cc].nick, _clients[invnick].nick, tokens[1]));
+		_channels[find_chan(tokens[1])].invited.push_back(invnick);
 	}
-	_clients[cc].send_to_user += irc::cEM(irc::ERR_CHANOPRIVSNEEDED(tokens[1]));
+	else
+		_clients[cc].send_to_user += SERVERNAME" USER NOT FOUND\r\n";
 }

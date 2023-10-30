7ff72ed38179b7ce14d8c069350844318fda0d28
pgorner <pgorner@student.42heilbronn.de>
2023-10-30 16:09:47 +0100

    stuff fixed

--- src/server/invite.cpp
+++ src/server/invite.cpp
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 20:58:34 by pgorner           #+#    #+#             */
-/*   Updated: 2023/10/30 16:08:39 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/10/30 16:09:42 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void Server::invite(std::vector<std::string> tokens, int cc)
 {
 	if(!check_params(tokens, 3)){_clients[cc].send_to_user += irc::cEM(irc::ERR_NEEDMOREPARAMS("INVITE")); return;};
 	if (!channelexists(tokens[1])){_clients[cc].send_to_user += SERVERNAME" Channel not found\r\n"; return;};
-	if (_clients[cc].admin == false && !client_oper(cc, find_chan(tokens[1]))){_clients[cc].send_to_user += irc::cEM(irc::ERR_NOPRIVILEGES()); return 0;}
+	if (_clients[cc].admin == false && !client_oper(cc, find_chan(tokens[1]))){_clients[cc].send_to_user += irc::cEM(irc::ERR_NOPRIVILEGES());}
 	int invnick = find_nick(tokens[2]);
 	if (invnick != -1)
 	{
